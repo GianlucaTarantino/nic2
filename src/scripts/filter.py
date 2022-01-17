22f2d2de0ed7704c45a3fc732c280f25d87386e3
Gianluca Tarantino <gianlutara@gmail.com>
2022-01-17 12:20:39 +0100

    Update filter parameters and remove useless variables

--- src/scripts/filter.py
+++ src/scripts/filter.py
@@ -1,26 +1,28 @@
-from utils_function import load_brainwaves, save_brainwaves
 from scipy import signal
 
-data = load_brainwaves("/home/gianluca/Programmazione/Progetti/BCI/data/raw/AcquiredBrainWave_2022-01-10_07-03-02_Alzati.csv")
+file_name = "15-Jan-2022T12:24:56.478956_1_L.csv"
+data = [float(e) for e in open("/home/gianluca/Programmazione/Progetti/BCI/data/raw/"+file_name).readlines()
+        if 5 > float(e) > -5]
 
-period = 4.0  # Total period of time of the detected signal
 sample_rate = 250.0  # Samples rate in Hz
-samples_number = int(period * sample_rate)  # Total number of samples in the period time
 nyq = 0.5 * sample_rate
 
-cutoff_low = 20  # Frequency of the low pass filter
-cutoff_high = 0.3  # Frequency of the high pass filter
+cutoff_low = 5  # Frequency of the low pass filter
+cutoff_high = 0.4  # Frequency of the high pass filter
 frequency_notch = 50  # Frequency of the notch filter
 
 # Applying low pass filter to remove certain frequencies lower than a threshold
-low_filtered_b, low_filtered_a = signal.butter(1, cutoff_low/nyq, btype='low', analog=False)
+# noinspection PyTupleAssignmentBalance
+low_filtered_b, low_filtered_a = signal.butter(1, cutoff_low/nyq, btype='low')
 low_filtered = signal.filtfilt(low_filtered_b, low_filtered_a, data)
+
 # Applying high pass filter to level the signal
-high_filtered_b, high_filtered_a = signal.butter(1, cutoff_high/nyq, btype='high', analog=False)
+# noinspection PyTupleAssignmentBalance
+high_filtered_b, high_filtered_a = signal.butter(1, cutoff_high/nyq, btype='high')
 high_filtered = signal.filtfilt(high_filtered_b, high_filtered_a, low_filtered)
+
 # Applying notch filter, to remove 50hz from signal
 notch_filtered_b, notch_filtered_a = signal.iirnotch(frequency_notch, 30.0, sample_rate)
 notch_filtered = signal.filtfilt(notch_filtered_b, notch_filtered_a, high_filtered)
 
-save_brainwaves("/home/gianluca/Programmazione/Progetti/BCI/data/processed/AcquiredBrainWave_2022-01-10_07-03-02_Alzati.csv",
-                notch_filtered)
+open("/home/gianluca/Programmazione/Progetti/BCI/data/processed/"+file_name, "w").write("\n".join(notch_filtered))
